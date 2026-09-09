ae230e40a9a0af42f5887646c77b275690c1f27f
Prince Kalu <tippified@gmail.com>
2026-09-09 16:57:46 -0500

    removed jobs

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ import {
   FiX,
   FiZap,
 } from "react-icons/fi";
-import CreatorJobOffers from "./components/CreatorJobOffers";
+
 import NativeBannerAd from "./components/NativeBannerAd";
 
 import ActivityTeaserSection from "./components/ActivityTeaserSection";
@@ -266,9 +266,7 @@ export default function HomePage(): ReactElement {
         <section id="banner">
           <BannerSlider />
         </section>
-        <section>
-          <CreatorJobOffers />
-        </section>
+
         <section id="discover">
           <ActivityTeaserSection />
         </section>
